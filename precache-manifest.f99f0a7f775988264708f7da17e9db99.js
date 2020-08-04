0fb755f02453d941ed42010fdfbaedb4a63d5e05
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-08-04 13:04:25 +0500

    Updates

---
+++ precache-manifest.f99f0a7f775988264708f7da17e9db99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6621fa3d55811dc578ddf931983379cf",
+    "url": "/adnanshahz2018/coursera-react/index.html"
+  },
+  {
+    "revision": "aed3e12325e4153705fa",
+    "url": "/adnanshahz2018/coursera-react/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "3925fd2e74f83e92f619",
+    "url": "/adnanshahz2018/coursera-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aed3e12325e4153705fa",
+    "url": "/adnanshahz2018/coursera-react/static/js/2.b727848c.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/adnanshahz2018/coursera-react/static/js/2.b727848c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3925fd2e74f83e92f619",
+    "url": "/adnanshahz2018/coursera-react/static/js/main.dcd71842.chunk.js"
+  },
+  {
+    "revision": "1beb5c4da0e38a26d36c",
+    "url": "/adnanshahz2018/coursera-react/static/js/runtime-main.1fac0e1d.js"
+  }
+]);
